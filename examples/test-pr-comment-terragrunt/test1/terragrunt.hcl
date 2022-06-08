b696ba42873c7742782c72f8c5c6e7b1a8725087
Steffen Tautenhahn <stevie-@users.noreply.github.com>
2022-06-08 10:15:32 +0200

    feat(test): test multiple files (#4)
    * feat(test): test multiple files
    
    Signed-off-by: Steffen Tautenhahn <stevie-@users.noreply.github.com>
    
    * fix(pr-comment): pass on multiple files
    
    Signed-off-by: Steffen Tautenhahn <stevie-@users.noreply.github.com>

--- examples/test-pr-comment-terragrunt/test1/terragrunt.hcl
+++ examples/test-pr-comment-terragrunt/test1/terragrunt.hcl
@@ -13,4 +13,5 @@ terraform {
 }
 
 inputs = {
+    filename = "test1"
 }
--- examples/test-pr-comment/main.tf
+++ examples/test-pr-comment/main.tf
@@ -1,5 +1,5 @@
-resource "local_file" "test" {
+resource "local_file" "txt" {
   content         = "Lorem Ipsum"
-  filename        = "test.txt"
+  filename        = format("%s.txt", var.filename)
   file_permission = "0644"
 }
